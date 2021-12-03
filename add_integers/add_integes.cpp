06766714791e1b7b17425308d8ec93660266ea32
Sam Bradbury <sam.bradbury@hotmail.com>
2021-12-04 08:57:12 +1100

    add two numbers

---
+++ add_integers/add_integes.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int n1, n2, total;
+    std::cout << "Enter first number: ";
+    std::cin >> n1;
+    std::cout << "Enter second number: ";
+    std::cin >> n2;
+    total = n1 + n2;
+    std::cout << "\n" << n1 << " + " << n2 << " = " << total << "\n";
+
+    return 0;
+}
