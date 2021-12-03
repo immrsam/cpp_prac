7696defe7fe174e2257e39457a302715189a3b13
Sam Bradbury <sam.bradbury@hotmail.com>
2021-12-04 08:52:00 +1100

    print_number

---
+++ print_number/print_number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int number;
+
+    std::cout << "Enter an integer: ";
+    std::cin >> number;
+    std::cout << "\nThe number entered was " << number << "\n";
+
+    return 0;
+}
