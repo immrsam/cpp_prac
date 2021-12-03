8520ec0f220d7b42e81553bb482f6938043ce68b
Sam Bradbury <sam.bradbury@hotmail.com>
2021-12-04 09:13:16 +1100

    datatype size

---
+++ 5-datatype_size/datatype_size.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+
+    std::cout << "Size of char: " << sizeof(char) << " bytes\n";
+    std::cout << "Size of int: " << sizeof(int) << " bytes\n";
+    std::cout << "Size of float: " << sizeof(float) << " bytes\n";
+    std::cout << "Size of double: " << sizeof(double) << " bytes\n";
+    std::cout << "Size of long: " << sizeof(long) << " bytes\n";
+    std::cout << "Size of short: " << sizeof(short) << " bytes\n";
+
+    std::cout << "Size of bool: " << sizeof(bool) << " bytes\n";
+    std::cout << "Size of wchar_t: " << sizeof(wchar_t) << " bytes\n";
+    std::cout << "Size of long double: " << sizeof(long double) << " bytes\n";
+
+    return 0;
+
+}
