918ae53f50157ce4fc2df65065a60819996b205a
Sam Bradbury <sam.bradbury@hotmail.com>
2021-12-07 10:31:44 +1100

    leap year

--- 12-leap_year/leap_year.cpp
+++ 12-leap_year/leap_year.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 
 int main() {
+    int year;
+    std::cout << "Enter a year: ";
+    std::cin >> year;
 
-
+    if (year % 4 == 0){
+        if (year % 100 == 0){
+            if (year % 400 == 0)
+                std::cout << year << " is a leap year\n";
+            else
+                std::cout << year << " is not leap year\n";
+        }
+        else
+            std::cout << year << " is a leap year\n";
+    }
+    else
+        std::cout << year << " is not a leap year\n";
+    
     return 0;
 }
