1756f8e38c30e88689f5aa6b6a18a24c6015c511
Sam Bradbury <sam.bradbury@hotmail.com>
2021-12-07 12:35:27 +1100

    find factorial

--- 13-find_factorial/find_factorial.cpp
+++ 13-find_factorial/find_factorial.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
 int main(){
+    int n;
+    long double factorial = 1.0;
+
+    std::cout << "Enter a positive integer: ";
+    std::cin >> n;
     
+    if(n <0)
+        std::cout << "No factorial for negative number.\n";
+    else {
+        for(int i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+        std::cout << "Factorial of " << n << " = " << factorial << "\n";
+    }
     return 0;
 }
