a2e8776d59a089e57297307631481adbabb34d10
Sam Bradbury <sam.bradbury@hotmail.com>
2021-12-18 21:50:25 +1100

    more linked list stuff

--- 00-linked_list/linked_list.cpp
+++ 00-linked_list/linked_list.cpp
@@ -7,9 +7,51 @@ class Node
     Node *next;
 };
 
+void print_list(Node * n)
+{
+    while(n != NULL){
+        std::cout << "Print LL: " << n->data << "\n";
+        n = n->next;
+    }
+}
+
+void push_top(Node ** head, int data){
+    Node *new_head = NULL;
+    new_head = new Node();
+    new_head->data = data;
+    new_head->next = *head;
+    *head = new_head;
+}
+
+void append_bottom(Node ** head, int data){
+    Node *new_bottom = NULL;
+    Node *bottom = *head;
+    new_bottom = new Node();
+    new_bottom->data = data;
+    while(bottom->next != NULL){
+        bottom = bottom->next;
+    }
+    bottom->next = new_bottom;
+}
+
+void reverse_linked_list(Node ** head)
+{
+    Node *current = *head;
+    Node *prev = NULL; 
+    Node *next = NULL;
+
+    while (current != NULL) 
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    *head = prev;
+}
+
 int main () {
     int count = 1; 
-    int arr[4] = {1, 2, 3, 4};
 
     Node *head;	
     Node *first = NULL;
@@ -34,19 +76,17 @@ int main () {
 
     head = first;
 
+
 	std::cout << "Linked List example\n";
-
-    while (head->next != NULL){
-        std::cout << "LL data: " << head->data << "\n";
-        head = head->next;
-        count++;
-    }
-    std::cout << "LL data: " << head->data << "\n";
-    std::cout << "Total list size = " << count << "\n";
-    std::cout << "Size = " << sizeof(Node) << " * " << count << " = " << sizeof(Node) * count << "\n";
-    std::cout << "Size of array = " << sizeof(arr) <<  "\n";
-
-
-
+    print_list(head);
+	std::cout << "----------------------\n";
+    push_top(&head, 55);
+    print_list(head);
+	std::cout << "----------------------\n";
+    append_bottom(&head, 123);
+    print_list(head);
+    reverse_linked_list(&head);
+	std::cout << "-----Reverse?--------------\n";
+    print_list(head);
     return 0;
 }
